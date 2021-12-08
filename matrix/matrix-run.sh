1ad9430c1492fd2604b436dbfbc857bc699b68d0
lorenzo <lorenztrubian@gmail.com>
2021-12-08 17:45:54 +0100

    adding matrix program ready to intensive test

---
+++ matrix/matrix-run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+declare -A matrix
+
+matrix[0,0]=2400
+matrix[0,1]=100
+matrix[0,2]=100
+matrix[1,0]=1200
+matrix[1,1]=200
+matrix[1,2]=100
+matrix[2,0]=800
+matrix[2,1]=300
+matrix[2,2]=100
+
+cd $PBS_O_WORKDIR
+
+# loading openmpi module
+module load openmpi-4.1.1+gnu-9.3.0
+# compilation of the matrix.cc file
+mpic++ matrix.cc -o matrix.o
+
+
+working_folder=matrix-$DUNO-$DDUE-$DTRE
+
+rm -r ${working_folder}
+mkdir ${working_folder}
+
+
+# how many iteration for measurement
+howmany=10
+
+for ((i = 0; i < 3; i++));
+do
+	# saving folder
+	sav_fol=./${working_folder}/matrix-${i}
+	mkdir sav_fol
+	for (( j = 0 ; j < $howmany ; j++ ));
+	do
+		mpirun -np 24 ./matrix.o $DUNO $DDUE $DTRE ${matrix[i,0]} ${matrix[i,1]} ${matrix[i,2]} > sav_fol/output-${j}.out
+	done
+done
---
+++ matrix/matrix-sta.sh
@@ -0,0 +1,62 @@
+#!bin/bash
+
+
+D1_vec=24
+
+qsub -Roe -l nodes=$1:ppn=$24 -l walltime=0:10:00 -q dssc -v DUNO=${D1_vec},DDUE=1,DTRE=1 ./matrix-run.sh
+
+declare -A D2_vec
+
+D2_vec[0,0]=12
+D2_vec[0,1]=2
+D2_vec[1,0]=2
+D2_vec[1,1]=12
+D2_vec[2,0]=6
+D2_vec[2,1]=4
+D2_vec[3,0]=4
+D2_vec[3,1]=6
+D2_vec[4,0]=3
+D2_vec[4,1]=8
+D2_vec[5,0]=8
+D2_vec[5,1]=3
+
+for ((i = 0; i < 5; i++));
+do
+	qsub -Roe -l nodes=$1:ppn=$24 -l walltime=0:10:00 -q dssc -v DUNO=${D2_vec[$i,0]},DDUE=${D2_vec[$i,1]},DTRE=1 ./matrix-run.sh
+done
+
+declare -A D3_vec
+
+D3_vec[0,0]=2
+D3_vec[0,1]=2
+D3_vec[0,2]=6
+D3_vec[1,0]=2
+D3_vec[1,1]=6
+D3_vec[1,2]=2
+D3_vec[2,0]=6
+D3_vec[2,1]=2
+D3_vec[2,2]=2
+D3_vec[3,0]=4
+D3_vec[3,1]=2
+D3_vec[3,2]=3
+D3_vec[4,0]=4
+D3_vec[4,1]=3
+D3_vec[4,2]=2
+D3_vec[5,0]=2
+D3_vec[5,1]=4
+D3_vec[5,2]=3
+D3_vec[6,0]=2
+D3_vec[6,1]=3
+D3_vec[6,2]=4
+D3_vec[7,0]=3
+D3_vec[7,1]=4
+D3_vec[7,2]=2
+D3_vec[8,0]=3
+D3_vec[8,1]=2
+D3_vec[8,2]=4
+
+
+for ((i = 0; i < 9; i++));
+do
+	qsub -Roe -l nodes=$1:ppn=$24 -l walltime=0:10:00 -q dssc -v DUNO=${D3_vec[i,0]},DDUE=${D3_vec[$i,1]},DTRE=${D3_vec[$i,2]} ./matrix-run.sh
+done
