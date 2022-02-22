e5a1808195d41cbd22fdd9f1bebb4ec9c42e611e
lorenzo <lorenztrubian@gmail.com>
2022-02-22 21:41:55 +0100

    bash script

---
+++ assignment2/bash-script/serial-test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This script compile and run the serial test for select and the kdtree building function
+# varying the number of dimensions received by the calling script
+
+
+cd $PBS_O_WORKDIR
+
+saving_folder=data/serial
+
+mkdir data
+mkdir data/serial
+
+module load gnu/9.3.0
+
+g++ 14_select_test.cc -o 14_select_test.o -std=c++11 -O3 -DNDIM=${DIMENSION}
+
+./14_select_test.o > ${saving_folder}/time-serial_${DIMENSION}.csv
+
+
---
+++ assignment2/bash-script/submit-serial.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script submit a job calling the serial-test.sh script
+# for each number of dimension it will submit a job that
+# 1. compile the code 14_select_test.cc with the right number of dimnesions ndim
+# 2. run that code collecting the data in "time-serial_ndim.csv" 
+
+dimension=( 2 3 4 5 )
+
+for ((i = 1; i <= 4; i++));
+do
+	ndim=${dimension[$i]}
+
+	qsub -l nodes=1:ppn=2 -l walltime=0:30:00 -v DIMENSION=${ndim} -q dssc_gpu ./serial-test.sh
+
+done
