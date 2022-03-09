65331f0f5e43d9fd73e3c000d60767ac38b6839d
lorenzo <lorenztrubian@gmail.com>
2022-03-09 17:58:31 +0100

    mpi, omp 32 processes and 2^32-1 kpoints

--- assignment2/bash-script/mpi-submit.sh
+++ assignment2/bash-script/mpi-submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script submit a job calling the mpi-test.sh script 
 
-# WARNINGS: present configuration to collect information for 2^31  kpoints
+# WARNINGS: present configuration to collect information for 2^31  kpoints but just for numberprocesses = 32
 
 dimension=( 2 3 4 5 )
 
@@ -9,7 +9,7 @@ numberprocesses=( 2 4 8 16 32 )
 
 numpoints=( 134217727 268435455 536870911 1073741823 2147483647 4294967295 )
 
-for ((k = 0; k < 5; k++)); # numberprocesses
+for ((k = 4; k < 5; k++)); # numberprocesses
 do
 	num_proc=${numberprocesses[$k]}
 
@@ -17,12 +17,12 @@ do
 	do
 		n_dim=${dimension[$i]}
 
-		for (( j = 4; j < 5; j++ )) # numpoints
+		for (( j = 5; j < 6; j++ )) # numpoints
 		do
 			num_points=${numpoints[$j]}
 			for (( prec = 0; prec < 2; prec++ )) # prec
 			do
-				qsub -l nodes=1:ppn=${num_proc} -l walltime=2:00:00 -v PREC=${prec},DIMENSION=${n_dim},NUMP=${num_points},NUMPROC=${num_proc} -q dssc_gpu ./mpi-test.sh
+				qsub -l nodes=1:ppn=${num_proc} -l walltime=4:00:00 -v PREC=${prec},DIMENSION=${n_dim},NUMP=${num_points},NUMPROC=${num_proc} -q dssc_gpu ./mpi-test.sh
 			done
 		done
 	done
