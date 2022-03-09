9fc60cff55dec93256a943b3dd81415f2406f02e
lorenzo <lorenztrubian@gmail.com>
2022-03-09 17:45:09 +0100

    omp, mpi 2^31-1 kpoints data collect

--- assignment2/bash-script/mpi-submit.sh
+++ assignment2/bash-script/mpi-submit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This script submit a job calling the mpi-test.sh script 
-# the if-else block is needed to NOT submit jobs that requires more than 30 minutes to complete
+
+# WARNINGS: present configuration to collect information for 2^31  kpoints
 
 dimension=( 2 3 4 5 )
 
@@ -8,25 +9,21 @@ numberprocesses=( 2 4 8 16 32 )
 
 numpoints=( 134217727 268435455 536870911 1073741823 2147483647 4294967295 )
 
-for ((k = 0; k < 5; k++));
+for ((k = 0; k < 5; k++)); # numberprocesses
 do
 	num_proc=${numberprocesses[$k]}
 
-	for ((i = 1; i < 4; i++));
+	for ((i = 0; i < 4; i++)); # dimension
 	do
 		n_dim=${dimension[$i]}
 
-		for (( j = 0; j < 6; j++ ))
+		for (( j = 4; j < 5; j++ )) # numpoints
 		do
 			num_points=${numpoints[$j]}
-
-			if [[ ${num_proc} -ge 8 ]] || [[ ${num_proc} -eq 2 && ${num_points} -lt 2147483647 ]] || [[ ${num_proc} -eq 4 && ${num_points} -le 2147483647 ]] 
-			then
-				for (( prec = 0; prec < 2; prec++ ))
-				do
-					qsub -l nodes=1:ppn=${num_proc} -l walltime=0:30:00 -v PREC=${prec},DIMENSION=${n_dim},NUMP=${num_points},NUMPROC=${num_proc} -q dssc_gpu ./mpi-test.sh
-				done
-			fi
+			for (( prec = 0; prec < 2; prec++ )) # prec
+			do
+				qsub -l nodes=1:ppn=${num_proc} -l walltime=2:00:00 -v PREC=${prec},DIMENSION=${n_dim},NUMP=${num_points},NUMPROC=${num_proc} -q dssc_gpu ./mpi-test.sh
+			done
 		done
 	done
 done
